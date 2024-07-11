ae159c26dd6aca3eff4ba4d9025f9b3c8188798d
leo <leonardoandrei41@gmail.com>
2024-07-11 17:55:18 -0300

    [Backend] Add create-products migration

---
+++ backend/src/database/migrations/20240711201226-create-products.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.CHAR(255),
+        allowNull: false,
+      },
+      image_url: {
+        type: Sequelize.CHAR(2048),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('products');
+  },
+};
